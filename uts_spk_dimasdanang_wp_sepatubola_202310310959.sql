186929e2deee968aa3b1aa0f1b3478048ff8a885
DimasAji8 <130625535+DimasAji8@users.noreply.github.com>
2023-10-31 11:13:39 +0700

    Add files via upload

---
+++ uts_spk_dimasdanang_wp_sepatubola_202310310959.sql
@@ -0,0 +1,11 @@
+INSERT INTO "07TPLP015".uts_spk_dimasdanang_wp_sepatubola ("Nama Sepatu",merek,"Berat Sepatu","Bahan Sepatu","Tampilan sepatu","Harga (Rp)",alternatif,"Nilai Akhir","Rank") VALUES
+	 ('Nike Mercurial Superfly','Nike','420 g','Synthetic Leather','Modern',2800000,2.2867258,0.086567,9),
+	 ('Adidas Predator','Adidas','430 g','Primeknit','Agresif',2520000,3.018329,0.114262804,3),
+	 ('Puma Future Z','Puma','450 g','EvoKNIT','Futuristik',2240000,3.0014982,0.11362566,4),
+	 ('New Balance Furon','New Balance','440 g','Synthetic Leather','Klasik',1680000,2.3920884,0.090555646,7),
+	 ('Under Armour Magnetico','Under Armour','420 g','Synthetic Leather','Klasik',1960000,2.590843,0.09807976,6),
+	 ('Mizuno Morelia Neo','Mizuno','430 g','Kangaroo Leather','Klasik',2520000,3.0811574,0.11664127,2),
+	 ('Diadora Brasil Italy','Diadora','440 g','Kangaroo Leather','Retro',1820000,2.8196418,0.10674124,5),
+	 ('Umbro Medusae','Umbro','430 g','Leather','Klasik',1540000,3.1532967,0.11937219,1),
+	 ('Kelme Precision','Kelme','450 g','Synthetic Leather','Minimalis',1400000,1.7523516,0.06633757,10),
+	 ('Joma Dribling','Joma','420 g','Synthetic Leather','Klasik',1260000,2.3197412,0.08781685,8);
